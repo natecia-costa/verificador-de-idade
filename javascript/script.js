5e23d47cc62452fa678bcb05f92f31b424c773c7
Natécia Costa <natecia_ilha@hotmail.com>
2026-01-15 16:17:57 -0300

    novo-projeto

---
+++ javascript/script.js
@@ -0,0 +1,44 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fAno.value.length == 0 || fAno.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'imagens/bebe-m.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/jovem-m.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/adulto-m.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/idoso-m.jpg')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <10) {
+                img.setAttribute('src', 'imagens/bebe-f.jpg')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/jovem-f.jpg')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/adulto-f.jpg')
+            } else {
+                img.setAttribute('src', 'imagens/idoso-f.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+        img.style.borderRadius = '50%'
+    }
+}
